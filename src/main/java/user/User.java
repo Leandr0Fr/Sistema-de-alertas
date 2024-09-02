1ab46062beef0618c261c8db335bc8212a5a6318
LeandroFr <leandro00fr@gmail.com>
2024-09-02 00:31:17 -0300

    add test  verify email user

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,4 +1,22 @@
 package user;
 
+import notifications.NotificationPanel;
+import utils.EmailValidatorImp;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String name;
+    private String email;
+    private NotificationPanel notificationPanel;
+    private List<String> topicsDenied;
+
+    public User(String name, String email) {
+        if (!EmailValidatorImp.isEmailValid(email)) throw new IllegalArgumentException("email no válido");
+        this.name = name;
+        this.email = email;
+        this.notificationPanel = new NotificationPanel();
+        this.topicsDenied = new ArrayList<>();
+    }
 }
---
+++ src/test/java/user/TestUser.java
@@ -0,0 +1,19 @@
+package user;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestUser {
+    private User user;
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidEmail() {
+        user = new User("test", "plainaddress");
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testValidEmail() {
+        user = new User("test", "plainaddress@gmail.com");
+    }
+}

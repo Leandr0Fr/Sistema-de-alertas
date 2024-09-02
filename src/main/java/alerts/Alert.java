7dcb522ae27687c1b810bcd1b8e15bea311ea555
LeandroFr <leandro00fr@gmail.com>
2024-09-01 21:46:25 -0300

    add Alert class

--- src/main/java/alerts/Alert.java
+++ src/main/java/alerts/Alert.java
@@ -1,6 +1,58 @@
 package alerts;
 
 
-public class Alert {
+import alerts.topics.Topic;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+
+public class Alert {
+    private LocalDateTime expirationDate;
+    private boolean isRead;
+    private boolean isUnique;
+    private Topic topic;
+
+    public Alert(Topic topic, boolean isUnique) {
+        if (topic == null) {
+            throw new NullPointerException("topic no puede ser declarado null");
+        }
+        this.topic = topic;
+        this.isUnique = isUnique;
+    }
+
+    public Alert(Topic topic, boolean isUnique, String expirationDate) {
+        if (topic == null) {
+            throw new NullPointerException("topic no puede ser declarado null");
+        }
+        this.expirationDate = parseTime(expirationDate);
+        this.isUnique = isUnique;
+        this.topic = topic;
+    }
+
+    private LocalDateTime parseTime(String expirationDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm");
+        try {
+            return LocalDateTime.parse(expirationDate, formatter);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
+    public boolean isUnique() {
+        return isUnique;
+    }
+
+    public void readAlert(){
+        this.isRead = true;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public LocalDateTime getExpirationDate() {
+        return expirationDate;
+    }
 }
---
+++ src/test/java/alerts/AlertTest.java
@@ -0,0 +1,63 @@
+package alerts;
+
+import alerts.topics.Topic;
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class AlertTest {
+    private Alert alert;
+    private Topic topic;
+
+    @Before
+    public void setUp() {
+        this.topic = new Topic("Test", "This topic is for testing");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNullTopic() {
+        this.alert = new Alert(null, true);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testVoidParseTime() {
+        this.alert = new Alert(topic, true, "");
+    }
+
+    @Test()
+    public void testValidParseTime() {
+        String datetimeExpected = "2018-12-18T09:12";
+        this.alert = new Alert(topic, true, "2018:12:18 09:12");
+
+        assertEquals(datetimeExpected, this.alert.getExpirationDate().toString());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidParseTime() {
+        this.alert = new Alert(topic, true, "invalidString");
+    }
+
+    @Test()
+    public void testReadAlert() {
+        this.alert = new Alert(topic, true);
+        boolean beforeIsReadStatus = this.alert.isRead();
+        this.alert.readAlert();
+        boolean afterIsReadStatus = this.alert.isRead();
+
+        assertTrue(afterIsReadStatus);
+        assertFalse(beforeIsReadStatus);
+    }
+
+    @Test()
+    public void testUniqueAlert() {
+        this.alert = new Alert(topic, true);
+        assertTrue(this.alert.isUnique());
+    }
+
+    @Test()
+    public void testNotUniqueAlert() {
+        this.alert = new Alert(topic, false);
+        assertFalse(this.alert.isUnique());
+    }
+}
